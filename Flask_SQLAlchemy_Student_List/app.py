ff898c167ed4dd7e9632893f865cb83b25a69753
Mike Jackson <perigran_falcon@yahoo.com>
2024-09-14 17:32:22 -0600

    Success!

--- Flask_SQLAlchemy_Student_List/app.py
+++ Flask_SQLAlchemy_Student_List/app.py
@@ -7,7 +7,7 @@ from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/database.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
